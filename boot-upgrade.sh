3d824fe6acdbe60b041c56cf161bfd51ba88bd9f
uavfly <pzhy521@163.com>
2023-01-02 19:56:03 +0800

    tools

---
+++ boot-upgrade.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+i="1"
+
+while(i=i)
+
+do
+
+	inotifywait -e modify /boot/firmware/vmlinuz
+
+	sleep 10s
+
+	cd /boot/firmware/
+
+	rm vmlinux.new
+
+	dd if=vmlinuz bs=1 skip=0000000 | zcat > vmlinux.new
+
+	rm vmlinux.old
+
+	cp vmlinux vmlinux.old
+
+	rm vmlinux
+
+	cp vmlinux.new vmlinuxw
+
+done
